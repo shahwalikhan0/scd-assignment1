e95a690b0b89e837928379a84d8617de61b4f586
Shah Wali Khan <shahwalikhan@ss01-ntb-029.intechww.com>
2023-09-14 08:36:10 +0500

    converted code into java

--- src/main/java/com/example/assignment1/Application.java
+++ src/main/java/com/example/assignment1/Application.java
@@ -3,11 +3,27 @@ package com.example.assignment1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the title of the book: ");
+        String title = scanner.nextLine();
+
+        System.out.print("Enter the author of the book: ");
+        String author = scanner.nextLine();
+
+        System.out.print("Enter the year of publication of the book: ");
+        int year = scanner.nextInt();
+
+        scanner.close();
+
+        Book book = new Book(title, author, year);
+        book.display();
     }
 
 }
